a9e6819c6d3fb29b71f80519ac5d089367dcc719
Elia Deppe <66836590+eliaCITS@users.noreply.github.com>
2020-07-16 08:54:25 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# List of Documetation for Assistance
+#   ♦ python keyword 'for' documentation: https://www.w3schools.com/python/python_for_loops.asp
+#   ♦ python keyword 'in' documentation: https://www.w3schools.com/python/ref_keyword_in.asp
+#   ♦ python function range() documentation: https://www.w3schools.com/python/ref_func_range.asp
+
+### put part 1 code below here ###
+
+
+
+
+
+### put part 2 code below here ###
+
+
+
+
+
+### put part 3 code below here ###
+
+
+
+
